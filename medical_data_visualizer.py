9b5c98b5f359cfe9afea6c4bbe0078b6a3a362f8
5555-master <85557181+5555-master@users.noreply.github.com>
2024-07-07 18:50:01 +0530

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,32 +4,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df =pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] =  ((df ["weight"]/(df["height"]/100)**2)>25).astype(int)
 
-# 3
+
+
+#3
+df["cholesterol"] =(df["cholesterol"]>1).astype(int)
+
+df["gluc"]=(df["gluc"]>1).astype(int)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    df_cat = pd.melt(df,id_vars=["cardio"],value_vars=["cholesterol","gluc","smoke","alco","active","overweight"])
     
 
-    # 7
+    # 6
+    df_cat=df_cat.groupby(["cardio","variable","value"]).size().reset_index()
+    df_cat=df_cat.rename(columns ={0:"total"})
 
-
-
-    # 8
-    fig = None
-
-
+   
+    graph=sns.catplot(data=df_cat,x="variable",y="total",kind="bar",hue="value",col="cardio")
+    # print(type(graph))
+    fig=graph.fig
+    
     # 9
     fig.savefig('catplot.png')
     return fig
@@ -38,23 +40,41 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
-    # 12
-    corr = None
+    df_heat = df[(df["height"] >= df["height"].quantile(0.025))&
+                 (df["height"]<= df["height"].quantile(0.975)) & 
+                 (df["weight"]>=df["weight"].quantile(0.025)) &
+                 (df["weight"]<=df["weight"].quantile(0.975))&
+                  (df["ap_lo"]<=df["ap_hi"]) ]
+   
+    # 12        
+    corr =df_heat.corr()
+    print(corr)
+    
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr,dtype=bool)) 
+    print(mask)
+    
 
 
 
     # 14
-    fig, ax = None
+    
+    
+    fig, ax = plt.subplots(figsize=(16, 9))
+    print(fig,ax)
+    sns.heatmap(corr, mask=mask,annot=True,fmt='.1f',square=True)
+   
+   
 
     # 15
+  
 
 
 
     # 16
     fig.savefig('heatmap.png')
     return fig
+
+
+draw_cat_plot()
